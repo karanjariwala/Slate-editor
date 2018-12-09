671947e6e809f67fed89a756800c049d6ea7a07b
karanjariwala <karanjariwala@gmail.com>
2018-12-09 17:27:21 +0530

    Updates

---
+++ precache-manifest.3a06a83f5935f3fb7cbf5534f9137453.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcba2a183b6aab45f997",
+    "url": "/Slate-editor/static/js/runtime~main.bcba2a18.js"
+  },
+  {
+    "revision": "cc984921febf64296576",
+    "url": "/Slate-editor/static/js/main.cc984921.chunk.js"
+  },
+  {
+    "revision": "6e262ab36ab3bdbf9c19",
+    "url": "/Slate-editor/static/js/1.6e262ab3.chunk.js"
+  },
+  {
+    "revision": "cc984921febf64296576",
+    "url": "/Slate-editor/static/css/main.0f9e7ca1.chunk.css"
+  },
+  {
+    "revision": "0290e1cb4bfcf8d9ebd037e8d280d25b",
+    "url": "/Slate-editor/index.html"
+  }
+];
